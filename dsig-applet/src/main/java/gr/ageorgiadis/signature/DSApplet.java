d1b51bcd55a66e74bce083683252b018fd93b090
Anestis Georgiadis <mranest@gmail.com>
2008-04-10 08:54:53 +0000

    Added Apache license text

--- dsig-applet/src/main/java/gr/ageorgiadis/signature/DSApplet.java
+++ dsig-applet/src/main/java/gr/ageorgiadis/signature/DSApplet.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2007-2008 Anestis Georgiadis
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
+
 package gr.ageorgiadis.signature;
 
 import gr.ageorgiadis.security.BrowserKeyStoreFactory;
@@ -193,6 +209,7 @@ public class DSApplet extends JApplet {
 	public String getAppletInfo() {
 		return "Digital Signature Applet - " +
 			application.getString("dsig-applet.version") + "\n" +
+			"http://dsig.sourceforge.net\n" +
 			"(c) Anestis Georgiadis, 2007-2008";
 	}
 	
--- dsig-applet/src/main/java/gr/ageorgiadis/signature/xmldsig/XMLDSigStrategy.java
+++ dsig-applet/src/main/java/gr/ageorgiadis/signature/xmldsig/XMLDSigStrategy.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2007-2008 Anestis Georgiadis
+ *  
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and 
+ * limitations under the License.
+ */
+
 package gr.ageorgiadis.signature.xmldsig;
 
 import gr.ageorgiadis.signature.ElementHandler;
@@ -48,24 +64,6 @@ import javax.xml.transform.stream.StreamResult;
 import org.apache.commons.codec.binary.Base64;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
-
-/* The XML Signature structure:
- * 
-<Signature ID?> 
-	<SignedInfo>
-		<CanonicalizationMethod/>
-		<SignatureMethod/>
-		(<Reference URI? >
-			(<Transforms>)?
-			<DigestMethod>
-			<DigestValue>
-		</Reference>)+
-	</SignedInfo>
-	<SignatureValue/> 
-	(<KeyInfo>)?
-	(<Object ID?>)*
-</Signature>
- */
 
 public class XMLDSigStrategy extends SignatureStrategy {
 	
